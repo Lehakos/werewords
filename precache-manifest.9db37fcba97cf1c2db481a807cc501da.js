4bd131fde0de3100d5f23eaf977b3c51cc7182b8
Aleksei Kosianenko <lehakos@gmail.com>
2020-05-12 20:15:51 +0300

    Build

---
+++ precache-manifest.9db37fcba97cf1c2db481a807cc501da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a172b4308362e41e018cf29f11d8293a",
+    "url": "/werewords/index.html"
+  },
+  {
+    "revision": "25afd04358a212c3c0d0",
+    "url": "/werewords/static/js/2.3a34557d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/werewords/static/js/2.3a34557d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86a61cf514d6247a8f06",
+    "url": "/werewords/static/js/main.fc58bfcb.chunk.js"
+  },
+  {
+    "revision": "01287d98bcc22a22dff3",
+    "url": "/werewords/static/js/runtime-main.edc344ef.js"
+  },
+  {
+    "revision": "ae391e95e999f2e4327b3bc313879f0c",
+    "url": "/werewords/static/media/question.ae391e95.svg"
+  }
+]);
